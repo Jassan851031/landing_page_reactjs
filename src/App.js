11928a1dab5007d1806eeeaf2abf0e9fb974c8ca
Jorge Leyva <jassan851031@gmail.com>
2020-06-30 14:38:40 -0400

    test ssh

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Cards from './components/cards';
 import Footer from './components/Footer';
 
 
-const App = (props) => {
+const App = () => {
     return (
         <>
             <Navbar />
@@ -16,14 +16,12 @@ const App = (props) => {
                     </div>
                 </div>
             </div>
-
             <div className="container">
                 <div className="row">
                     <div className="col-md-12">
                         <Jumbotron title="A Warm Welcome!" text="Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa, ipsam, eligendi, in quo sunt possimus non incidunt odit vero aliquid similique quaerat nam nobis illo aspernatur vitae fugiat numquam repellat." btn="Learn more" />
                     </div>
                 </div>
-
                 <div className="row">
                     <div className="col-md-3">
                         <Cards img="https://picsum.photos/id/237/200/300" title="Card title" text="Some quick example text to build on the card title and make up the bulk of the card's content." />
